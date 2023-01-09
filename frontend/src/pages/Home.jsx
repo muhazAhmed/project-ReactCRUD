494a3a099d36a1dd5955e09821d90a3ab7b1c30f
Muhaz <muhazvla313@gmail.com>
2023-01-09 10:17:59 +0530

    Changed button feature

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,17 +1,29 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
+import React, { useContext } from "react";
+import { Link } from "react-router-dom";
+import { AuthContext } from "../context/AuthContext";
 
 const Home = () => {
+  const { currentUser } = useContext(AuthContext);
   return (
-    <div className='home-main' data-aos="zoom-in">
-    <div className='home' >
-      <h1>Welcome To The React CRUD Op. Page</h1>
+    <div className="home-main" data-aos="zoom-in">
+      <div className="home">
+        <h1>Welcome To The React CRUD Op. Page</h1>
+      </div>
+      {currentUser ? (
+        <Link
+          to="/user/dashboard"
+          style={{ textDecoration: "none" }}
+          data-aos="slide-up"
+        >
+          <button className="btn-home">Dashboard</button>
+        </Link>
+      ) : (
+        <Link className="link" to="/login">
+          <button className="btn-home">Proceed</button>
+        </Link>
+      )}
     </div>
-    <Link  to="/register" data-aos="slide-up">
-      <button className='btn-home' >Proceed</button>
-    </Link>
-    </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
