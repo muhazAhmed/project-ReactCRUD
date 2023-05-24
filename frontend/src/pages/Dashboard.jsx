70d19316605af276561ab82c7f3a314d78dbc988
Muhaz Ahmed <muhazvla313@gmail.com>
2023-05-24 16:11:47 +0530

    added loading animation

--- frontend/src/pages/Dashboard.jsx
+++ frontend/src/pages/Dashboard.jsx
@@ -1,6 +1,7 @@
 import React, { useContext, useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import axios from "axios";
+import HashLoader from "react-spinners/HashLoader";
 import { AuthContext } from "../context/AuthContext";
 import Modal from "../components/Model";
 import Navbar from "../components/Navbar";
@@ -12,21 +13,34 @@ const Dashboard = () => {
   const [selectedEmployeeId, setSelectedEmployeeId] = useState(null);
   const [currentPage, setCurrentPage] = useState(1);
   const [itemsPerPage] = useState(5);
+  const [isLoading, setIsLoading] = useState(false); // New state variable
 
   const { currentUser } = useContext(AuthContext);
 
   const fetchEmp = async () => {
-    const result = await axios.get("https://react-crud-v3am.onrender.com/api/user/emp");
-    setEmp(result.data);
-  };
-
-  const updateEmpData = async () => {
+    setIsLoading(true); // Set loading state to true
     try {
-      const result = await axios.get("https://react-crud-v3am.onrender.com/api/user/emp");
+      const result = await axios.get(
+        "https://react-crud-v3am.onrender.com/api/user/emp"
+      );
       setEmp(result.data);
     } catch (error) {
       console.log(error.message);
     }
+    setIsLoading(false); // Set loading state to false after data fetch
+  };
+
+  const updateEmpData = async () => {
+    setIsLoading(true); // Set loading state to true
+    try {
+      const result = await axios.get(
+        "https://react-crud-v3am.onrender.com/api/user/emp"
+      );
+      setEmp(result.data);
+    } catch (error) {
+      console.log(error.message);
+    }
+    setIsLoading(false); // Set loading state to false after data update
   };
 
   useEffect(() => {
@@ -73,7 +87,10 @@ const Dashboard = () => {
                 <i className="fa-solid fa-user-plus"></i>{" "}
               </button>
             </Link>
-            <h1 data-aos="zoom-in">Welcome back <span style={{color : "red"}}>{currentUser.User.username}</span></h1>
+            <h1 data-aos="zoom-in">
+              Welcome back{" "}
+              <span style={{ color: "red" }}>{currentUser.User.username}</span>
+            </h1>
           </div>
           <div className="search" data-aos="zoom-in">
             <i className="fa-sharp fa-solid fa-magnifying-glass"></i>
@@ -85,48 +102,52 @@ const Dashboard = () => {
             />
           </div>
 
-          <table className="table">
-            <thead data-aos="slide-up">
-              <tr>
-                <th>No. </th>
-                <th>Name </th>
-                <th>Age </th>
-                <th>Salary P/A</th>
-                <th>Designation </th>
-                <th>Update </th>
-                <th>Delete </th>
-              </tr>
-            </thead>
-            <tbody data-aos="slide-up">
-              {currentItems.map((item, index) => (
-                <tr key={item[1]._id}>
-                  <td>{index + 1}</td>
-                  <td>{item[1].Name}</td>
-                  <td>{item[1].Age}</td>
-
-                  <td>{item[1].Salary}</td>
-                  <td>{item[1].Designation}</td>
-                  <td data-label="Update">
-                    {currentUser.username === emp.Name && (
-                      <Link to={`/employ/edit/${item[1]._id}`}>
-                        <i className="fa-regular fa-pen-to-square"></i>
-                      </Link>
-                    )}
-                  </td>
-                  <td data-label="Delete">
-                    <Link
-                      onClick={() => {
-                        setSelectedEmployeeId(item[1]._id);
-                        setModalOpen(true);
-                      }}
-                    >
-                      <i className="fa-solid fa-trash"></i>
-                    </Link>
-                  </td>
+          {isLoading ? ( // Display loading animation if isLoading is true
+            <div style={{marginLeft : "40rem", marginTop : "2rem"}}><HashLoader color="#3EDFFF" loading={isLoading} size={180} /></div>
+          ) : (
+            <table className="table">
+              <thead data-aos="slide-up">
+                <tr>
+                  <th>No. </th>
+                  <th>Name </th>
+                  <th>Age </th>
+                  <th>Salary P/A</th>
+                  <th>Designation </th>
+                  <th>Update </th>
+                  <th>Delete </th>
                 </tr>
-              ))}
-            </tbody>
-          </table>
+              </thead>
+              <tbody data-aos="slide-up">
+                {currentItems.map((item, index) => (
+                  <tr key={item[1]._id}>
+                    <td>{index + 1}</td>
+                    <td>{item[1].Name}</td>
+                    <td>{item[1].Age}</td>
+
+                    <td>{item[1].Salary}</td>
+                    <td>{item[1].Designation}</td>
+                    <td data-label="Update">
+                      {currentUser.username === emp.Name && (
+                        <Link to={`/employ/edit/${item[1]._id}`}>
+                          <i className="fa-regular fa-pen-to-square"></i>
+                        </Link>
+                      )}
+                    </td>
+                    <td data-label="Delete">
+                      <Link
+                        onClick={() => {
+                          setSelectedEmployeeId(item[1]._id);
+                          setModalOpen(true);
+                        }}
+                      >
+                        <i className="fa-solid fa-trash"></i>
+                      </Link>
+                    </td>
+                  </tr>
+                ))}
+              </tbody>
+            </table>
+          )}
 
           <div className="pagination-btn">
             <div>
@@ -136,7 +157,8 @@ const Dashboard = () => {
                 className={isFirstPage ? "disabled" : ""}
               >
                 &lt;&lt;
-              </button>&nbsp;&nbsp;
+              </button>
+              &nbsp;&nbsp;
               <button
                 onClick={() => handlePageChange(currentPage - 1)}
                 disabled={isFirstPage}
@@ -152,7 +174,8 @@ const Dashboard = () => {
                 className={isLastPage ? "disabled" : ""}
               >
                 Next Page ({currentPage + 1})
-              </button>&nbsp;&nbsp;
+              </button>
+              &nbsp;&nbsp;
               <button
                 onClick={() => handlePageChange(totalPages)}
                 disabled={isLastPage}
